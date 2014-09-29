fec4cdb28ffb67a45f5058b15bf033c6c223b60b
Shevchik <Shev4ik.den@gmail.com>
2014-09-29 13:15:35 +0400

    Remap multiblockchange too. Now only material remapping table and fix block change packet left

--- src/main/java/net/o2gaming/carbon/protocolmodifier/ProtocolBlockListener.java
+++ src/main/java/net/o2gaming/carbon/protocolmodifier/ProtocolBlockListener.java
@@ -43,6 +43,7 @@ public class ProtocolBlockListener {
 					if (ProtocolLibrary.getProtocolManager().getProtocolVersion(event.getPlayer()) == 47) {
 						return;
 					}
+					//chunk packet is split to 16 columns 16*16*16, if column doesn't have any blocks - it is not sent
 					int blocksNumber = 4096 * getChunkSectionNumber(event.getPacket().getIntegers().read(2));
 					byte[] data = event.getPacket().getByteArrays().read(1);
 					for (int i = 0; i < blocksNumber; i++) {
@@ -64,6 +65,7 @@ public class ProtocolBlockListener {
 					if (ProtocolLibrary.getProtocolManager().getProtocolVersion(event.getPlayer()) == 47) {
 						return;
 					}
+					//the same as map chunk, but we have multiple chunks data store in inflatedbuffers
 					byte[][] inflatedBuffers = event.getPacket().getSpecificModifier(byte[][].class).read(0);
 					int[] chunkSectionsData = event.getPacket().getIntegerArrays().read(2);
 					for (int chunkNumber = 0; chunkNumber < inflatedBuffers.length; chunkNumber++) {
@@ -89,11 +91,38 @@ public class ProtocolBlockListener {
 					if (ProtocolLibrary.getProtocolManager().getProtocolVersion(event.getPlayer()) == 47) {
 						return;
 					}
+					//just modify the block field to the one we need
+					//TODO: looks like this packet is shared between multiple players so we need to create a copy and send it instead of modifying original one
 					net.minecraft.server.v1_7_R4.Block block = event.getPacket().getSpecificModifier(net.minecraft.server.v1_7_R4.Block.class).read(0);
 					int id = net.minecraft.server.v1_7_R4.Block.getId(block);
 					if (replacements[id] != -1) {
 						net.minecraft.server.v1_7_R4.Block newBlock = net.minecraft.server.v1_7_R4.Block.getById(replacements[id]);
 						event.getPacket().getSpecificModifier(net.minecraft.server.v1_7_R4.Block.class).write(0, newBlock);
+					}
+				}
+			}
+		);
+
+		ProtocolLibrary.getProtocolManager().addPacketListener(
+			new PacketAdapter(
+				PacketAdapter.params(plugin, PacketType.Play.Server.MULTI_BLOCK_CHANGE)
+			) {
+				@Override
+				public void onPacketSending(PacketEvent event) {
+					if (ProtocolLibrary.getProtocolManager().getProtocolVersion(event.getPlayer()) == 47) {
+						return;
+					}
+					//the format is: 4 bytes (1st byte - ZX (or XZ, i don't remember and that doesn't matter here), 2nd byte - Y, 3,4th bytes - id 12 bits + data 4 bits)
+					byte[] bytes = event.getPacket().getByteArrays().read(0);
+					for (int i = 2; i < bytes.length; i+= 4) {
+						int iddata = ((bytes[i] & 0xFF) << 8) | (bytes[i + 1] & 0xFF);
+						int id = iddata >> 4;
+						int data = iddata & 0xF;
+						if (replacements[id] != -1) {
+							int newiddata = replacements[id] << 4 | data;
+							bytes[i] = (byte) (newiddata >> 8);
+							bytes[i + 1] = (byte) newiddata;
+						}
 					}
 				}
 			}
