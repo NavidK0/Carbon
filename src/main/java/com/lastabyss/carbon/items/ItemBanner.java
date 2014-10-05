c4fe4806f4b5deb5040ea52dae14b91059189466
Shevchik <Shev4ik.den@gmail.com>
2014-10-05 13:28:52 +0400

    Fix banners place check

--- src/main/java/com/lastabyss/carbon/items/ItemBanner.java
+++ src/main/java/com/lastabyss/carbon/items/ItemBanner.java
@@ -31,52 +31,31 @@ public class ItemBanner extends ItemBlock {
 		if (blockFace == CraftBlock.blockFaceToNotch(BlockFace.DOWN)) {
 			return false;
 		}
+		if (paramWorld.getType(x, y, z).getMaterial().isReplaceable()) {
+			return false;
+		}
 		org.bukkit.block.Block bblock = paramWorld.getWorld().getBlockAt(x, y, z);
 		bblock = bblock.getRelative(CraftBlock.notchToBlockFace(blockFace));
 		x = bblock.getX();
 		y = bblock.getY();
 		z = bblock.getZ();
-		if (!paramWorld.getType(x, y, z).getMaterial().isReplaceable()) {
+		if (!paramEntityHuman.a(x, y, z, blockFace, paramItemStack) || !paramWorld.getType(x, y, z).getMaterial().isReplaceable()) {
 			return false;
 		}
-		/*var4 = var4.getRelative(var5);
-		if (!var2.a(var4, var5, var1)) {
-			return false;
-		} else {*/
-			if (blockFace == CraftBlock.blockFaceToNotch(BlockFace.UP)) {
-				int data = MathHelper.floor(((paramEntityHuman.yaw + 180.0F) * 16.0F / 360.0F) + 0.5D) & 15;
-				paramWorld.setTypeAndData(x, y, z, Carbon.injector().standingBannerBlock, data, 2);
-			} else {
-				//paramWorld.setBlockAt(x, y, z, Carbon.injector().wallBannerBlock, 0, 2);
-			}
-
-			--paramItemStack.count;
-			TileEntity te = paramWorld.getTileEntity(x, y, z);
-			if (te instanceof TileEntityBanner) {
-				((TileEntityBanner) te).setItemValues(paramItemStack);
-			}
-
-			return true;
-		//}
-	}
-
-	/*public String a(ItemStack var1) {
-		String var2 = "item.banner.";
-		akv var3 = this.h(var1);
-		var2 = var2 + var3.d() + ".name";
-		return LocaleI18n.get(var2);
-	}
-
-	private akv h(ItemStack var1) {
-		NBTCompoundTag var2 = var1.a("BlockEntityTag", false);
-		akv var3 = null;
-		if (var2 != null && var2.hasKey("Base")) {
-			var3 = akv.a(var2.getInt("Base"));
+		if (blockFace == CraftBlock.blockFaceToNotch(BlockFace.UP)) {
+			int data = MathHelper.floor(((paramEntityHuman.yaw + 180.0F) * 16.0F / 360.0F) + 0.5D) & 15;
+			paramWorld.setTypeAndData(x, y, z, Carbon.injector().standingBannerBlock, data, 2);
 		} else {
-			var3 = akv.a(var1.getWearout());
+			//paramWorld.setBlockAt(x, y, z, Carbon.injector().wallBannerBlock, 0, 2);
 		}
 
-		return var3;
-	}*/
+		--paramItemStack.count;
+		TileEntity te = paramWorld.getTileEntity(x, y, z);
+		if (te instanceof TileEntityBanner) {
+			((TileEntityBanner) te).setItemValues(paramItemStack);
+		}
+
+		return true;
+	}
 
 }
