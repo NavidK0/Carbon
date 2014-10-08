401bc7c00a62354c1f3f5f6dfa6d0706b2111d51
pupnewfster <richard@freimer.com>
2014-10-08 16:13:10 -0400

    Update so can Merge + endermite spawning

--- src/main/java/com/lastabyss/carbon/generator/CarbonEntityGenerator.java
+++ src/main/java/com/lastabyss/carbon/generator/CarbonEntityGenerator.java
@@ -4,7 +4,14 @@ import java.lang.reflect.Field;
 import java.util.List;
 
 import net.minecraft.server.v1_7_R4.BiomeBase;
+import net.minecraft.server.v1_7_R4.BiomeBigHills;
+import net.minecraft.server.v1_7_R4.BiomeForest;
+import net.minecraft.server.v1_7_R4.BiomeJungle;
 import net.minecraft.server.v1_7_R4.BiomeMeta;
+import net.minecraft.server.v1_7_R4.BiomePlains;
+import net.minecraft.server.v1_7_R4.BiomeSavanna;
+import net.minecraft.server.v1_7_R4.BiomeSwamp;
+import net.minecraft.server.v1_7_R4.BiomeTaiga;
 
 import com.lastabyss.carbon.Carbon;
 import com.lastabyss.carbon.entity.EntityGuardian;
@@ -25,6 +32,7 @@ public class CarbonEntityGenerator {
             injectGuardianSpawner();
     }
     
+    @SuppressWarnings("unchecked")
     private void injectGuardianSpawner() {
         //spawns guardians in deep oceans
         try {
@@ -34,7 +42,6 @@ public class CarbonEntityGenerator {
                         for (Field list : BiomeBase.class.getDeclaredFields())
                             if (list.getType().getSimpleName().equals(List.class.getSimpleName())) {
                                 list.setAccessible(true);
-                                @SuppressWarnings("unchecked")
                                 List<BiomeMeta> metaList = (List<BiomeMeta>) list.get(f.get(null));
                                 metaList.add(new BiomeMeta(EntityGuardian.class, 1, 2, 4));
                              }
@@ -43,22 +50,22 @@ public class CarbonEntityGenerator {
             }
     }
     
-    private void injectRabbitSpawner() {
-        //adds rabbits to spawn 
-        try {
-            for (Field f : BiomeBase.class.getDeclaredFields())
-                if (f.getType().getSimpleName().equals(BiomeBase.class.getSimpleName()))
-                    if (f.get(null) != null && (f.getName().contains("PLAINS") || f.getName().contains("FOREST") || f.getName().contains("EXTREME_HILLS") ||
-                        f.getName().contains("JUNGLE") || f.getName().contains("TAIGA") || f.getName().contains("SAVANNA")))
-                        for (Field list : BiomeBase.class.getDeclaredFields())
-                            if (list.getType().getSimpleName().equals(List.class.getSimpleName())) {
-                                list.setAccessible(true);
-                                @SuppressWarnings("unchecked")
-                                List<BiomeMeta> metaList = (List<BiomeMeta>) list.get(f.get(null));
-                                metaList.add(new BiomeMeta(EntityRabbit.class, 10, 3, 3));
-                             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    @SuppressWarnings("unchecked")
+	public void injectRabbitSpawner() {
+		// adds rabbits to spawn
+		try {
+			Field atField = net.minecraft.server.v1_7_R4.BiomeBase.class.getDeclaredField("at");
+			atField.setAccessible(true);
+			for (BiomeBase biomeBase : BiomeBase.getBiomes()) {
+				if (biomeBase != null) {
+					if (biomeBase instanceof BiomePlains || biomeBase instanceof BiomeBigHills || biomeBase instanceof BiomeForest ||
+						biomeBase instanceof BiomeTaiga || biomeBase instanceof BiomeSwamp || biomeBase instanceof BiomeJungle ||
+						biomeBase instanceof BiomeSavanna)
+							((List<BiomeMeta>) atField.get(biomeBase)).add(new BiomeMeta(EntityRabbit.class, 10, 3, 3));
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
