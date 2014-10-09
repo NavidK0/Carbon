9ad8b8dadc1426e22e8f4443444cb9b03bdf260e
Shevchik <Shev4ik.den@gmail.com>
2014-10-10 01:22:56 +0400

    I prefer my minecraft nickname

--- src/main/java/com/lastabyss/carbon/Carbon.java
+++ src/main/java/com/lastabyss/carbon/Carbon.java
@@ -164,7 +164,7 @@ public class Carbon extends JavaPlugin {
   private void printHelpMenu(CommandSender sender) {
       sender.sendMessage(ChatColor.DARK_GRAY + "--=======" + ChatColor.DARK_RED + "Carbon" + ChatColor.DARK_GRAY + "=======--");
       sender.sendMessage(ChatColor.DARK_GRAY + "Version:" + ChatColor.DARK_RED + this.getDescription().getVersion()); // Reads out the version of the plugin.yml
-      sender.sendMessage(ChatColor.DARK_GRAY + "Main Authors:" + ChatColor.DARK_RED + " NavidK0, shevchik_, Aust1n46"); // Sorted like in plugin.yml
+      sender.sendMessage(ChatColor.DARK_GRAY + "Main Authors:" + ChatColor.DARK_RED + " NavidK0, _Shevchik_, Aust1n46"); // Sorted like in plugin.yml
       sender.sendMessage(ChatColor.DARK_GRAY + "Other contributors:" + ChatColor.DARK_RED + "pupnewfster, Stefenatefun, Jikoo, Wombosvideo, mcmonkey4eva, sickray34s"); // Sorted by activity (Names mainly from GitHub)
       sender.sendMessage(ChatColor.DARK_GRAY + "IRC:" + ChatColor.DARK_RED + " irc.esper.net");
       sender.sendMessage(ChatColor.DARK_GRAY + "Channel:" + ChatColor.DARK_RED + " #Carbon");
