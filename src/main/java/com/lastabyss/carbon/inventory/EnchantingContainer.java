fcbe6016d65682d3baca649bbc8af0b1c7b68c14
Shevchik <Shev4ik.den@gmail.com>
2014-10-10 01:02:43 +0400

    Display one known enchantment for 1.8 client

--- src/main/java/com/lastabyss/carbon/inventory/EnchantingContainer.java
+++ src/main/java/com/lastabyss/carbon/inventory/EnchantingContainer.java
@@ -11,12 +11,15 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.enchantment.EnchantItemEvent;
 import org.bukkit.event.enchantment.PrepareItemEnchantEvent;
 
+import com.lastabyss.carbon.utils.Utilities;
+
 import net.minecraft.server.v1_7_R4.Blocks;
 import net.minecraft.server.v1_7_R4.Enchantment;
 import net.minecraft.server.v1_7_R4.EnchantmentInstance;
 import net.minecraft.server.v1_7_R4.EnchantmentManager;
 import net.minecraft.server.v1_7_R4.EntityHuman;
 import net.minecraft.server.v1_7_R4.EntityPlayer;
+import net.minecraft.server.v1_7_R4.ICrafting;
 import net.minecraft.server.v1_7_R4.IInventory;
 import net.minecraft.server.v1_7_R4.ItemStack;
 import net.minecraft.server.v1_7_R4.Items;
@@ -31,6 +34,7 @@ public class EnchantingContainer extends net.minecraft.server.v1_7_R4.ContainerE
 	private int x;
 	private int y;
 	private int z;
+	private int[] nonRandomEnchants = new int[] { -1, -1, -1 };
 
 	public EnchantingContainer(PlayerInventory playerInventory, World world, int x, int y, int z) {
 		super(playerInventory, world, x, y, z);
@@ -42,8 +46,38 @@ public class EnchantingContainer extends net.minecraft.server.v1_7_R4.ContainerE
 	}
 
 	@Override
+	public void addSlotListener(ICrafting icrafting) {
+		super.addSlotListener(icrafting);
+		icrafting.setContainerData(this, 0, this.costs[0]);
+		icrafting.setContainerData(this, 1, this.costs[1]);
+		icrafting.setContainerData(this, 2, this.costs[2]);
+		if (Utilities.getProtocolVersion(player.getBukkitEntity()) == Utilities.CLIENT_1_8_PROTOCOL_VERSION) {
+			icrafting.setContainerData(this, 3, (int) (this.f & -16));
+			icrafting.setContainerData(this, 4, this.nonRandomEnchants[0]);
+			icrafting.setContainerData(this, 5, this.nonRandomEnchants[1]);
+			icrafting.setContainerData(this, 6, this.nonRandomEnchants[2]);
+		}
+	}
+
+	@Override
+	public void b() {
+		super.b();
+		for (int i = 0; i < this.listeners.size(); ++i) {
+			ICrafting icrafting = (ICrafting) this.listeners.get(i);
+			icrafting.setContainerData(this, 0, this.costs[0]);
+			icrafting.setContainerData(this, 1, this.costs[1]);
+			icrafting.setContainerData(this, 2, this.costs[2]);
+			if (Utilities.getProtocolVersion(player.getBukkitEntity()) == Utilities.CLIENT_1_8_PROTOCOL_VERSION) {
+				icrafting.setContainerData(this, 3, (int) (this.f & -16));
+				icrafting.setContainerData(this, 4, this.nonRandomEnchants[0]);
+				icrafting.setContainerData(this, 5, this.nonRandomEnchants[1]);
+				icrafting.setContainerData(this, 6, this.nonRandomEnchants[2]);
+			}
+		}
+	}
+
+	@Override
 	public void a(IInventory iinventory) {
-		// TODO: add known enchantment calculation
 		if (iinventory == this.enchantSlots) {
 			ItemStack itemstack = iinventory.getItem(0);
 			if (itemstack != null) {
@@ -77,14 +111,30 @@ public class EnchantingContainer extends net.minecraft.server.v1_7_R4.ContainerE
 				}
 				for (int i = 0; i < 3; i++) {
 					this.costs[i] = EnchantmentManager.a(random, i, bookShelfs, itemstack);
+					this.nonRandomEnchants[i] = -1;
+					if (this.costs[i] < i + 1) {
+						this.costs[i] = 0;
+					}
 				}
+
+				for (int i = 0; i < 3; ++i) {
+					if (this.costs[i] > 0) {
+						List<EnchantmentInstance> list = getEnchantsToAdd(itemstack, i, costs[i]);
+						if (list != null && !list.isEmpty()) {
+							EnchantmentInstance enchantmentWithLevel = (EnchantmentInstance) list.get(this.random.nextInt(list.size()));
+							this.nonRandomEnchants[i] = enchantmentWithLevel.enchantment.id | enchantmentWithLevel.level << 8;
+						}
+					}
+				}
+
 				CraftItemStack item = CraftItemStack.asCraftMirror(itemstack);
 				PrepareItemEnchantEvent event = new PrepareItemEnchantEvent(this.player.getBukkitEntity(), getBukkitView(), this.world.getWorld().getBlockAt(this.x, this.y, this.z), item, this.costs, bookShelfs);
 				event.setCancelled(!itemstack.x());
 				this.world.getServer().getPluginManager().callEvent(event);
 				if (event.isCancelled()) {
-					for (bookShelfs = 0; bookShelfs < 3; bookShelfs++) {
-						this.costs[bookShelfs] = 0;
+					for (int i = 0; i < 3; i++) {
+						this.costs[i] = 0;
+						this.nonRandomEnchants[i] = -1;
 					}
 					return;
 				}
@@ -92,6 +142,7 @@ public class EnchantingContainer extends net.minecraft.server.v1_7_R4.ContainerE
 			} else {
 				for (int i = 0; i < 3; i++) {
 					this.costs[i] = 0;
+					this.nonRandomEnchants[i] = -1;
 				}
 			}
 		}
@@ -103,7 +154,7 @@ public class EnchantingContainer extends net.minecraft.server.v1_7_R4.ContainerE
 		ItemStack itemstack = this.enchantSlots.getItem(0);
 		if ((this.costs[i] > 0) && (itemstack != null) && ((entityhuman.expLevel >= this.costs[i]) || (entityhuman.abilities.canInstantlyBuild))) {
 			if (!this.world.isStatic) {
-				List<?> list = EnchantmentManager.b(random, itemstack, this.costs[i]);
+				List<?> list = getEnchantsToAdd(itemstack, i, this.costs[i]);
 				if (list == null) {
 					list = new ArrayList<Object>();
 				}
@@ -149,6 +200,16 @@ public class EnchantingContainer extends net.minecraft.server.v1_7_R4.ContainerE
 			return true;
 		}
 		return false;
+	}
+
+	private List<EnchantmentInstance> getEnchantsToAdd(ItemStack itemstack, int i, int cost) {
+		random.setSeed(this.f + i);
+		@SuppressWarnings("unchecked")
+		List<EnchantmentInstance> list = EnchantmentManager.b(random, itemstack, costs[i]);
+		if (itemstack.getItem() == Items.BOOK && list != null && list.size() > 1) {
+			list.remove(this.random.nextInt(list.size()));
+		}
+		return list;
 	}
 
 }
