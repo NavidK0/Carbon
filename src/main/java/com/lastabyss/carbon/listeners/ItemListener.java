924bfc737c6f28c9948057880eed923be86a2a1a
Jikoo <Jikoo.Games@gmail.com>
2014-10-12 11:57:46 -0400

    Respect gamerules

--- src/main/java/com/lastabyss/carbon/listeners/ItemListener.java
+++ src/main/java/com/lastabyss/carbon/listeners/ItemListener.java
@@ -45,7 +45,8 @@ public class ItemListener implements Listener {
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onTeleport(PlayerTeleportEvent e) {
-        if(e.getCause().equals(TeleportCause.ENDER_PEARL) && plugin.getConfig().getBoolean("mobSpawning.endermites", true)) {
+        if (e.getCause() == TeleportCause.ENDER_PEARL && plugin.getConfig().getBoolean("mobSpawning.endermites", true)
+                && e.getTo().getWorld().getAllowMonsters()) {
             Random rand = new Random();
             if(rand.nextInt(100) < 5) {//5% probability of spawning
             	EntityEndermite ender = new EntityEndermite(((CraftWorld) e.getFrom().getWorld()).getHandle());
@@ -56,45 +57,45 @@ public class ItemListener implements Listener {
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onEntityKilled(EntityDeathEvent e) {
-        if (plugin.getConfig().getBoolean("options.sheep.dropMutton", true)) {
-            if(e.getEntityType().equals(EntityType.SHEEP)) {
-                if(e.getEntity() instanceof Ageable) {
-                    Ageable entity = (Ageable) e.getEntity();
-                    if(entity.isAdult()) {
-                        boolean fireaspect = false;
-                        int looting = 1;
-                        if(entity.getLastDamageCause() instanceof EntityDamageByEntityEvent) {
-                            EntityDamageByEntityEvent dEvent = (EntityDamageByEntityEvent) entity.getLastDamageCause();
-                            if(dEvent.getDamager() != null && dEvent.getDamager() instanceof Player) {
-                                ItemStack hand = ((Player) dEvent.getDamager()).getItemInHand();
-                                fireaspect = hand.containsEnchantment(Enchantment.FIRE_ASPECT);
-                                if(hand.containsEnchantment(Enchantment.LOOT_BONUS_MOBS))
-                                    looting += hand.getEnchantmentLevel(Enchantment.LOOT_BONUS_MOBS);
-                                if(looting < 1) //Incase a plugin sets an enchantment level to be negative
-                                    looting = 1;
-                            } else if(dEvent.getDamager() != null && dEvent.getDamager() instanceof Arrow) {
-                                Arrow a = (Arrow) dEvent.getDamager();
-                                if(a.getFireTicks() > 0)
-                                    fireaspect = true;
-                            }
-                        }
-                        if(entity.getLastDamageCause().getCause().equals(DamageCause.FIRE_TICK) || entity.getLastDamageCause().getCause().equals(DamageCause.FIRE) ||
-                                entity.getLastDamageCause().getCause().equals(DamageCause.LAVA) || fireaspect)
-                            e.getDrops().add(new ItemStack(Carbon.injector().cookedMuttonItemMat, random.nextInt(2) + 1 + random.nextInt(looting)));
-                        else
-                            e.getDrops().add(new ItemStack(Carbon.injector().muttonItemMat, random.nextInt(2) + 1 + random.nextInt(looting)));
-                    }
-                }
+        if (e.getEntity().getType() != EntityType.SHEEP || !plugin.getConfig().getBoolean("options.sheep.dropMutton", true)
+                || !e.getEntity().getWorld().isGameRule("doMobLoot") || !(e.getEntity() instanceof Ageable)) {
+            return;
+        }
+        Ageable entity = (Ageable) e.getEntity();
+        if (!entity.isAdult()) {
+            return;
+        }
+        boolean fireaspect = false;
+        int looting = 1;
+        if (entity.getLastDamageCause() instanceof EntityDamageByEntityEvent) {
+            EntityDamageByEntityEvent dEvent = (EntityDamageByEntityEvent) entity.getLastDamageCause();
+            if (dEvent.getDamager() != null && dEvent.getDamager() instanceof Player) {
+                ItemStack hand = ((Player) dEvent.getDamager()).getItemInHand();
+                fireaspect = hand.containsEnchantment(Enchantment.FIRE_ASPECT);
+                if (hand.containsEnchantment(Enchantment.LOOT_BONUS_MOBS))
+                    looting += hand.getEnchantmentLevel(Enchantment.LOOT_BONUS_MOBS);
+                if (looting < 1) //Incase a plugin sets an enchantment level to be negative
+                    looting = 1;
+            } else if (dEvent.getDamager() != null && dEvent.getDamager() instanceof Arrow) {
+                Arrow a = (Arrow) dEvent.getDamager();
+                if (a.getFireTicks() > 0)
+                    fireaspect = true;
             }
         }
+        if (entity.getLastDamageCause().getCause() == DamageCause.FIRE_TICK || entity.getLastDamageCause().getCause() == DamageCause.FIRE
+                || entity.getLastDamageCause().getCause() == DamageCause.LAVA || fireaspect)
+            e.getDrops().add(new ItemStack(Carbon.injector().cookedMuttonItemMat, random.nextInt(2) + 1 + random.nextInt(looting)));
+        else
+            e.getDrops().add(new ItemStack(Carbon.injector().muttonItemMat, random.nextInt(2) + 1 + random.nextInt(looting)));
     }
 
     @EventHandler
     public void onCreeperDeath(EntityDeathEvent e) {
         LivingEntity entity = e.getEntity();
-        if (entity.getLastDamageCause() == null || entity.getLastDamageCause().getCause() == null
+        if (entity.getLastDamageCause().getCause() != DamageCause.ENTITY_EXPLOSION
                 || !plugin.getConfig().getBoolean("options.creeper.dropMobHead", true)
-                || entity.getLastDamageCause().getCause() != DamageCause.ENTITY_EXPLOSION
+                || !entity.getWorld().isGameRule("doMobLoot")
+                || entity.getLastDamageCause() == null || entity.getLastDamageCause().getCause() == null
                 || !(entity.getLastDamageCause() instanceof EntityDamageByEntityEvent)) return;
         EntityDamageByEntityEvent damageEvent = (EntityDamageByEntityEvent) entity.getLastDamageCause();
         if (damageEvent.getDamager() == null || damageEvent.getDamager().getType() != EntityType.CREEPER
