48c78d6499e7f4a8c28df93b2f455e2d77fb6b67
Shevchik <Shev4ik.den@gmail.com>
2014-10-14 23:30:29 +0400

    Add non compilable class so not so smart people won't try to use that for now if they don't understand the risk.

---
+++ src/main/java/com/lastabyss/carbon/CompileError.java
@@ -0,0 +1,8 @@
+package com.lastabyss.carbon;
+
+//this class server only for one purpose - do not let the build server compile this shit before we really ready to release at least an alpha version of this
+public class CompileError {
+
+	CompileError
+
+}
