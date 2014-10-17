bd80ef41a8afbb4b3dbc903fc87498746d4505ab
Shevchik <Shev4ik.den@gmail.com>
2014-10-17 10:04:45 +0400

    This should be like this

--- src/main/java/org/bukkit/craftbukkit/v1_7_R4/inventory/CraftMetaItem.java
+++ src/main/java/org/bukkit/craftbukkit/v1_7_R4/inventory/CraftMetaItem.java
@@ -665,23 +665,22 @@ class CraftMetaItem implements ItemMeta, Repairable {
 		return false;
 	}
 
-        @Override
+	@Override
 	public final String toString() {
 		return (String) SerializableMeta.classMap.get(getClass()) + "_META:" + serialize();
 	}
 
-	private final Spigot spigot = new Spigot();
+	private final ItemMeta.Spigot spigot = new ItemMeta.Spigot();
 
-	public Spigot spigot() {
+	public ItemMeta.Spigot spigot() {
 		return this.spigot;
 	}
-        
+
     class Spigot {
         private boolean unbreakable;
 
         public void setUnbreakable(boolean setUnbreakable) {
                 this.unbreakable = setUnbreakable;
-
         }
 
         public boolean isUnbreakable() {
